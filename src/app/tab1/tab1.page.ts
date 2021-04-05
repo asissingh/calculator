8c12c5bd8c30cb8c86240926ef9dc7bf997a561e
asissingh <ashishsinghrajput35@gmail.com>
2021-04-05 17:00:17 +0530

    final Upload

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,7 +12,24 @@ export class Tab1Page {
   constructor() {}
 
   num(n) {
-    this.input += n;
+    let last = this.input[this.input.length - 1];
+    let secLast = this.input[this.input.length - 2];
+    if (this.input.length == 1 && this.input[0] == '0') {
+      this.input = n;
+    } else if (
+      last == '0' &&
+      (secLast == '+' ||
+        secLast == '-' ||
+        secLast == '×' ||
+        secLast == '÷' ||
+        secLast == '%' ||
+        secLast == '^')
+    ) {
+      this.input = this.input.slice(0, this.input.length - 1);
+      this.input += n;
+    } else {
+      this.input += n;
+    }
   }
 
   closeOne() {
@@ -39,39 +56,39 @@ export class Tab1Page {
     }
     this.FinalOutPut = eval(b);
   }
-clearAnd(){
-  this.FinalAll();
-  this.find = true;
-
-}
+  clearAnd() {
+    this.FinalAll();
+    this.find = true;
+  }
   operators(n) {
-    let b = '';
-    let last = this.input[this.input.length - 1];
-    if (
-      last == '+' ||
-      last == '-' ||
-      last == '×' ||
-      last == '÷' ||
-      last == '^'
-    ) {
-      if(n == "%"){
-        b = this.input.slice(0, this.input.length - 1);
-        let d = +eval(b);
-      this.input = d / 100 + '';
+    if (this.input != '') {
+      let b = '';
+      let last = this.input[this.input.length - 1];
+      if (
+        last == '+' ||
+        last == '-' ||
+        last == '×' ||
+        last == '÷' ||
+        last == '^'
+      ) {
+        if (n == '%') {
+          b = this.input.slice(0, this.input.length - 1);
+          let d = +eval(b);
+          this.input = d / 100 + '';
+        } else {
+          last.replace('÷', '/');
+          b = this.input.slice(0, this.input.length - 1);
+          b += n;
+          this.input = b;
+        }
+      } else if (n == '%') {
+        let d = +eval(this.input);
+
+        this.input = d / 100 + '';
+      } else {
+        this.input += n;
+        ``;
       }
-      else{
-      last.replace('÷', '/');
-      b = this.input.slice(0, this.input.length - 1);
-      b += n;
-      this.input = b;}
-    } else if (n == '%') {
-      let d = +eval(this.input);
-
-      this.input = d / 100 + '';
-    } else {
-      this.input += n;
-      ``;
     }
-
   }
 }
